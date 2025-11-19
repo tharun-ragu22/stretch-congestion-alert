b2aceca0b6875bf250e93ee26666c1492951707d
Tharun Ragulan <t2ragula@uwaterloo.ca>
2025-11-19 18:10:42 -0500

    remove console logs

--- frontend/db.ts
+++ frontend/db.ts
@@ -1,13 +1,6 @@
 const { Pool } = require('pg');
 // require ('dotenv').config();
 
-// 1. Check if environment variables are loaded
-console.log('--- DB Connection Debug ---');
-console.log('PG_HOST:', process.env.PG_HOST);
-console.log('PG_USER:', process.env.PG_USER);
-console.log('PG_NAME:', process.env.PG_NAME);
-console.log('PG_PORT:', process.env.PG_PORT);
-console.log('---------------------------');
 
 const ConnectionPool = new Pool({
     host: process.env.PG_HOST,
