6c6baf0117a0824394b8ff71f8ce7b78c41a039d
Tharun Ragulan <t2ragula@uwaterloo.ca>
2025-11-18 23:04:55 -0500

    can make postgres requests and display results on dashboard

--- frontend/app/[userid]/dashboard/page.tsx
+++ frontend/app/[userid]/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ConnectionPool from "@/db";
 
 interface DashboardProps {
   params: {
@@ -6,14 +7,55 @@ interface DashboardProps {
   };
 }
 
-const UserDashboardPage: React.FC<DashboardProps> = async ({ params } : DashboardProps) => {
+interface Point {
+  x: string;
+  y: string;
+}
+
+interface PointRow {
+  beginpoint : Point;
+  endpoint: Point;
+}
+
+const UserDashboardPage: React.FC<DashboardProps> = async ({
+  params,
+}: DashboardProps) => {
   const { userid } = await params;
+
+  const test = await ConnectionPool.query(
+    `SELECT beginpoint, endpoint FROM intersections where userid = '${userid}';`
+  );
+  console.log(test.rows);
+
+  
 
   return (
     <div>
       Current user: {userid}
+      <table>
+        <thead>
+          <tr>
+            <th>Begin</th>
+            <th>End</th>
+          </tr>
+        </thead>
+        <tbody>
+          {test.rows.map((points: PointRow) => {
+            console.log(points)
+            const {beginpoint, endpoint} = points;
+            return (
+            <tr>
+              <td>
+                {beginpoint.x},{beginpoint.y}
+              </td>
+              <td>
+                {endpoint.x},{endpoint.y}
+              </td>
+            </tr>)})}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
 export default UserDashboardPage;
